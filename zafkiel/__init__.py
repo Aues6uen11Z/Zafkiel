708d569c0282c05c639b42c3330d8f81c822416d
Aues6uen11Z <79563000+Aues6uen11Z@users.noreply.github.com>
2023-11-28 19:36:48 +0800

    Bump version 0.0.3 -> 0.0.4

--- zafkiel/__init__.py
+++ zafkiel/__init__.py
@@ -8,7 +8,7 @@ from zafkiel.timer import Timer
 from zafkiel.report import simple_report
 from zafkiel.logger import logger
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 
 G.register_custom_device(WindowsPlatform)
